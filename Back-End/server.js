e96f18ccbed315105a30461adf240967920b139f
Samueeeelll <samuel_silvany@estudante.sesisenai.org.br>
2026-06-15 16:59:55 -0300

    15-06-26

---
+++ Back-End/server.js
@@ -0,0 +1,12 @@
+import express from  'express'
+
+app.use = (express.json())
+
+const use = express()
+
+app.listen(prompt, () =>{
+    console.log('O servidor está rodando')
+});
+
+
+const port = 3000;
